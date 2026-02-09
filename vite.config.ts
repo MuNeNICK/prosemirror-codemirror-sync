3aaf9697f998f47489975f95311ef861fff150be
munenick <mune0127@munenick.me>
2026-02-10 04:12:30 +0900

    add: pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/prosemirror-split-editor/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
